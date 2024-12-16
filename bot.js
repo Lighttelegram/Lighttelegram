f121a5eba97915f8b939e46829822b6cfdc216c6
Lighttelegram <yosfhussein805@gmail.com>
2024-12-16 13:38:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot('AAHFZ2v4ZnP72XNj0zPYI3zHR7bP2i9A6TU', { polling: true });
+const TelegramBot = require('8088416514:AAHFZ2v4ZnP72XNj0zPYI3zHR7bP2i9A6TU');
+const bot = new TelegramBot('8088416514:AAHFZ2v4ZnP72XNj0zPYI3zHR7bP2i9A6TU', { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
