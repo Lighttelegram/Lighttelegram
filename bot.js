8e9c5cf8b419ef7ccd19528aa1762df304a78729
Lighttelegram <yosfhussein805@gmail.com>
2024-12-16 13:40:20 +0300

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-const TelegramBot = require('8088416514:AAHFZ2v4ZnP72XNj0zPYI3zHR7bP2i9A6TU');
-const bot = new TelegramBot('8088416514:AAHFZ2v4ZnP72XNj0zPYI3zHR7bP2i9A6TU', { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Hello! Welcome to the bot.');
-});
