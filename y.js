17cba29753cdc4a8b2868d8cc3862e1dec1124f8
Lighttelegram <yosfhussein805@gmail.com>
2024-12-16 13:34:24 +0300

    Create y.js

---
+++ y.js
@@ -0,0 +1,7 @@
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot('8088416514:AAHFZ2v4ZnP72XNj0zPYI3zHR7bP2i9A6TU', { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Hello! Welcome to the bot.');
+});
